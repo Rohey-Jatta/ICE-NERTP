69b54e9fecbb6e9660618bf4ba27d5464cd57d2e
Rohey-Jatta <rjatta03@gmail.com>
2026-05-13 16:50:27 +0000

    update fakerphp/faker

--- database/seeders/PartySeeder.php
+++ database/seeders/PartySeeder.php
@@ -17,20 +17,28 @@ class PartySeeder extends Seeder
         }
 
         $parties = [
-            ['name' => "National People's Party", 'abbreviation' => 'NPP'],
-            ['name' => 'United Democratic Party', 'abbreviation' => 'UDP'],
-            ['name' => 'Gambia Democratic Congress', 'abbreviation' => 'GDC'],
-            ['name' => 'People\'s Democratic Organisation for Independence and Socialism', 'abbreviation' => 'PDOIS'],
-            ['name' => 'National Union Party', 'abbreviation' => 'NUP'],
-            ['name' => 'Independent', 'abbreviation' => 'IND'],
+            ['name' => "National People's Party",                                              'abbreviation' => 'NPP'],
+            ['name' => 'United Democratic Party',                                              'abbreviation' => 'UDP'],
+            ['name' => 'Gambia Democratic Congress',                                           'abbreviation' => 'GDC'],
+            ['name' => "People's Democratic Organisation for Independence and Socialism",      'abbreviation' => 'PDOIS'],
+            ['name' => 'National Union Party',                                                 'abbreviation' => 'NUP'],
+            ['name' => 'Independent',                                                          'abbreviation' => 'IND'],
         ];
 
         foreach ($parties as $p) {
-            PoliticalParty::create(array_merge([
-                'election_id' => $electionId,
-                'is_active' => true,
-                'slug' => Str::slug($p['name']),
-            ], $p));
+            PoliticalParty::firstOrCreate(
+                [
+                    'election_id'  => $electionId,
+                    'abbreviation' => $p['abbreviation'],
+                ],
+                [
+                    'name'      => $p['name'],
+                    'slug'      => Str::slug($p['name']),
+                    'is_active' => true,
+                ]
+            );
         }
+
+        $this->command->info('PartySeeder: parties created/verified.');
     }
 }
