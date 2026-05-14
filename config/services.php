ca9e83f6fe3fecc0cebf85ed4edf1f534506bf82
Rohey-Jatta <rjatta03@gmail.com>
2026-05-14 11:06:06 +0000

    update the sms service to twilio

--- config/services.php
+++ config/services.php
@@ -2,18 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
@@ -37,15 +25,25 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | SMS Service (Africa's Talking)
+    | SMS Service Configuration
     |--------------------------------------------------------------------------
+    | Driver options: 'twilio', 'africastalking', 'log'
+    | Use 'log' for local development (code appears in storage/logs/laravel.log)
     */
+    'sms' => [
+        'driver' => env('SMS_DRIVER', 'log'),
+    ],
+
+    'twilio' => [
+        'sid'   => env('TWILIO_SID'),
+        'token' => env('TWILIO_AUTH_TOKEN'),
+        'from'  => env('TWILIO_FROM'),
+    ],
+
     'africastalking' => [
         'username' => env('AFRICASTALKING_USERNAME', 'sandbox'),
-        'api_key' => env('AFRICASTALKING_API_KEY'),
-        'from' => env('AFRICASTALKING_FROM', 'IEC_NERTP'),
+        'api_key'  => env('AFRICASTALKING_API_KEY'),
+        'from'     => env('AFRICASTALKING_FROM', 'IEC_NERTP'),
     ],
 
 ];
-
-
